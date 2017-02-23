de0377439c9d8e60af320dbf782fed242539d5d4
dalisoft <dalisoft@mail.ru>
2017-02-23 23:29:41 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+import TWEEN from './src/TWEEN';
+import { Easing } from './src/TWEEN.Easing';
+import Tween from './src/TWEEN.Tween';
+
+TWEEN.Easing = Easing;
+
+TWEEN.Tween = Tween;
+
+export default TWEEN;
