1267a65dd810463eb54167b04b1c008008a6ea85
dalisoft <dalisoft@mail.ru>
2017-02-24 20:16:49 +0500

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-import TWEEN from './src/TWEEN';
-import { Easing } from './src/TWEEN.Easing';
-import Tween from './src/TWEEN.Tween';
-
-TWEEN.Easing = Easing;
-
-TWEEN.Tween = Tween;
-
-export default TWEEN;
