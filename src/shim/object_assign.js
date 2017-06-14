bf0a1469f1a9dfc9e330e0983b620893afdc7b3e
dalisoft <dalisoft@mail.ru>
2017-06-14 22:16:13 +0500

    Create object_assign.js
    fix(shim/object.assign): fixes object.assign

--- src/shim/object_assign.js
+++ src/shim/object_assign.js
@@ -1,6 +1,5 @@
 if ( Object.assign === undefined ) {
-	Object.assign = (...args) => {
-		let first = args.shift();
+	Object.assign = (first, ...args) => {
 			args.map(obj => {
 				for ( let p in obj ) {
 					first[p] = obj[p];
