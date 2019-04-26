d8da174ab98df9b0963524dc5796ffbca44f1e84
Simon <34153549+seIncorp@users.noreply.github.com>
2019-04-26 15:45:34 +0200

    Additionaly check in TWEEN.Tween.to function
    I added additionally check in TWEEN.Tween.to, if the this.object is equal to this._valuesEnd. If not all excess parameters are removed.

--- src/Tween.js
+++ src/Tween.js
@@ -395,6 +395,22 @@ class Tween {
       }
     }
 
+    let temp1 = Object.keys(this._valuesEnd)
+    let temp2 = Object.keys(this.object)
+    
+    if(temp1.length > temp2.length){
+      temp1.forEach(el => {
+        if(!temp2.includes(el))
+          delete this._valuesEnd[el];
+      })
+    }
+    
+    if(temp2.length > temp1.length){
+      temp2.forEach(el => {
+        if(!temp1.includes(el))
+          delete this.object[el]
+      })
+    
     return this
   }
 
